9c963fdf167329a6716eb51f45dfc40a442b4b42
chengshaozhuang <chengsz7890@gmail.com>
2024-09-28 20:35:20 +0800

    规范方法命名

--- src/main/java/org/suda/config/ArgumentResolverBeanFactoryConfiguration.java
+++ src/main/java/org/suda/config/ArgumentResolverBeanFactoryConfiguration.java
@@ -1,10 +1,8 @@
 package org.suda.config;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
 /**
  * ArgumentResolveBeanFactory配置类
@@ -12,12 +10,11 @@ import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandl
  * @dateTime 2024-07-12 06:53
  */
 @Configuration
-@ConditionalOnClass({RequestMappingHandlerAdapter.class})
 public class ArgumentResolverBeanFactoryConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    public ArgumentResolverBeanFactory IBeanFactory() {
+    public ArgumentResolverBeanFactory argumentResolverBeanFactory() {
         return new ArgumentResolverBeanFactory();
     }
 
