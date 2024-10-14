053a4262c9549f489adc4df6140f34780555e91a
chengshaozhuang <chengsz7890@gmail.com>
2024-10-14 23:31:13 +0800

    新增接口带有‘RequestParam’注解纯字符串参数去空格单元测试

--- src/test/java/org/suda/handler/StringMethodArgumentHandlerIntegrationTests.java
+++ src/test/java/org/suda/handler/StringMethodArgumentHandlerIntegrationTests.java
@@ -1,11 +1,17 @@
 package org.suda.handler;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.suda.common.Constant;
-import org.suda.controller.StringMethodArgumentHandlerTestController;
+import org.suda.common.Result;
 import org.suda.config.ArgumentResolverConfiguration;
 import org.suda.config.SudaProperties;
 import javax.annotation.Resource;
@@ -18,7 +24,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * 字符串安全检查单元测试
  * @author chengshaozhuang
 */
-@SpringBootTest(classes = {StringMethodArgumentHandlerTestController.class,
+@SpringBootTest(classes = {
+        StringMethodArgumentHandlerIntegrationTests.StringMethodArgumentHandlerTestController.class,
         ArgumentResolverConfiguration.class,
         MockMvcAutoConfiguration.class})
 class StringMethodArgumentHandlerIntegrationTests {
@@ -34,9 +41,47 @@ class StringMethodArgumentHandlerIntegrationTests {
     }
 
     @Test
-    void testRequestParamStringTrimWithNoAnnotation() throws Exception {
-        String url = Constant.PREFIX_SERVLET_PATH + "/requestParamWithStringTrimWithNoAnnotation";
-        String name = "chengshaozhuang    ";
-        this.mockMvc.perform(get(url).param("name", name)).andExpect(jsonPath("$.data").value(name.trim()));
+    void testRequestParamStringTrimWithoutAnnotation() throws Exception {
+        String url = Constant.PREFIX_SERVLET_PATH + "/requestParamStringTrimWithoutAnnotation";
+        testRequestParamStringTrim(url);
+    }
+
+    @Test
+    void testRequestParamStringTrimByRequestParamAnnotation() throws Exception {
+        String url = Constant.PREFIX_SERVLET_PATH + "/requestParamStringTrimByRequestParamAnnotation";
+        testRequestParamStringTrim(url);
+    }
+
+
+    private void testRequestParamStringTrim(String url) throws Exception {
+        String name = "  chengshaozhuang    ";
+        mockMvc.perform(get(url).param("name", name)).andExpect(jsonPath("$.data").value(name.trim()));
+    }
+
+    @RestController
+    @RequestMapping(Constant.PREFIX_SERVLET_PATH)
+    static class StringMethodArgumentHandlerTestController {
+
+        private final Logger logger = LoggerFactory.getLogger(getClass());
+
+        @RequestMapping(value = "/requestParamStringTrimWithoutAnnotation", method = RequestMethod.GET)
+        public Result<?> requestParamStringTrimWithoutAnnotation(String name) {
+            printLog(name);
+            return Result.OK(name);
+        }
+
+        @RequestMapping(value = "/requestParamStringTrimByRequestParamAnnotation", method = RequestMethod.GET)
+        public Result<?> requestParamStringTrimByRequestParamAnnotation(@RequestParam String name) {
+            printLog(name);
+            return Result.OK(name);
+        }
+
+        private void printLog(String name) {
+            if (name == null) {
+                logger.info("入参：null");
+            } else {
+                logger.info("入参：'{}'，长度：{}", name, name.length());
+            }
+        }
     }
 }
