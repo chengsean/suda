9ebc6c7c5aa054fcb9f9dbfa0fff7d1f2f3322b4
chengshaozhuang <chengsz7890@gmail.com>
2024-10-16 15:10:05 +0800

    修复单元测试发现的问题

--- src/main/java/org/suda/SecurityRequestParamMethodArgumentResolver.java
+++ src/main/java/org/suda/SecurityRequestParamMethodArgumentResolver.java
@@ -1,5 +1,6 @@
 package org.suda;
 
+import org.springframework.ui.Model;
 import org.springframework.web.multipart.support.MultipartResolutionDelegate;
 import org.suda.handler.MethodArgumentHandler;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
@@ -14,11 +15,10 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.Part;
 import java.util.Collection;
-import java.util.Map;
 
 /**
  * {@link RequestParamMethodArgumentResolver}扩展类，检测和处理{@link RequestMapping}注解的方法中带有{@link RequestParam}注解的{@link String}对象、
- * 以及无任何注解的字符串参数类型，包括：{@link String}，包装类中的{@link String}、{@link Map<Object,String>}、{@link Collection<String>}，
+ * 以及无任何注解的字符串参数类型，包括：{@link String}，包装类中的{@link String}、{@link Model}、{@link Collection<String>}，
  * 文件类型，如：{@link MultipartFile}、{@link Part}等。
  * @author chengshaozhuang
  * @dateTime 2023-07-29 20:13
@@ -40,7 +40,7 @@ public class SecurityRequestParamMethodArgumentResolver extends RequestParamMeth
         Object arg = super.resolveName(name, parameter, webRequest);
         HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
         if (MultipartResolutionDelegate.isMultipartArgument(parameter)) {
-            fileMethodArgumentHandler.securityChecks(arg, servletRequest, parameter);
+            return fileMethodArgumentHandler.securityChecks(arg, servletRequest, parameter);
         }
         return stringMethodArgumentHandler.securityChecks(arg, servletRequest, parameter);
     }
