c14e1214ae04c76be7fb579f7e85f12ce66f63d3
chengshaozhuang <chengsz7890@gmail.com>
2024-10-16 15:24:29 +0800

    新增接口带有'RequestParam'注解的Collection参数的字符串去空格单元测试

--- src/test/java/org/suda/SecurityRequestParamMethodArgumentResolverTests.java
+++ src/test/java/org/suda/SecurityRequestParamMethodArgumentResolverTests.java
@@ -13,53 +13,69 @@ import org.springframework.web.bind.annotation.RestController;
 import org.suda.common.Constant;
 import org.suda.common.Result;
 import org.suda.config.ArgumentResolverConfiguration;
-import org.suda.config.SudaProperties;
-
 import javax.annotation.Resource;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Objects;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * description
  *
  * @author chengshaozhuang
- * @dateTime 2024-10-15 23:56
  */
-
 @SpringBootTest(classes = {
-        SecurityRequestParamMethodArgumentResolverTests.SecurityRequestParamMethodArgumentResolverTestController.class,
+        SecurityRequestParamMethodArgumentResolverTests
+                .SecurityRequestParamMethodArgumentResolverTestController.class,
         ArgumentResolverConfiguration.class,
         MockMvcAutoConfiguration.class})
 class SecurityRequestParamMethodArgumentResolverTests {
 
     @Resource
     private MockMvc mockMvc;
-
-    @Test
-    void testRequestParamStringTrimIsEnabledWithNoAnnotation() {
-        SudaProperties properties = new SudaProperties();
-        properties.getChars().setTrimEnabled(true);
-        assertThat(properties.getChars().isTrimEnabled()).isTrue();
-    }
+    static final String NAME_KEY = "name";
+    static final String NAME_VALUE = " chengshaozhuang   ";
+    static final String ID_KEY = "id";
+    static final String COLL_NAME = "collection";
 
     @Test
     void testRequestParamStringTrimWithoutAnnotation() throws Exception {
+        // 测试接口无注解字符串参数去空格是否有效
         String url = Constant.PREFIX_SERVLET_PATH + "/requestParamStringTrimWithoutAnnotation";
         testRequestParamStringTrim(url);
     }
 
     @Test
     void testRequestParamStringTrimByRequestParamAnnotation() throws Exception {
+        // 测试接口有'RequestParam'注解字符串参数去空格是否有效
         String url = Constant.PREFIX_SERVLET_PATH + "/requestParamStringTrimByRequestParamAnnotation";
         testRequestParamStringTrim(url);
     }
 
+    @Test
+    void testRequestParamStringTrimByRequestParamAnnotationWithName() throws Exception {
+        // 测试接口有'RequestParam'注解、注解带有'name'参数去空格是否有效
+        String url = Constant.PREFIX_SERVLET_PATH + "/requestParamStringTrimByRequestParamAnnotationWithName";
+        final int id = 1024;
+        mockMvc.perform(get(url).param(NAME_KEY, NAME_VALUE)
+                .param(ID_KEY, Objects.toString(id))).andExpect(jsonPath("$.data.name").value(NAME_VALUE.trim()))
+                .andExpect(jsonPath("$.data.id").value(id));
+    }
 
     private void testRequestParamStringTrim(String url) throws Exception {
-        String name = "  chengshaozhuang    ";
-        mockMvc.perform(get(url).param("name", name)).andExpect(jsonPath("$.data").value(name.trim()));
+        mockMvc.perform(get(url).param(NAME_KEY, NAME_VALUE)).andExpect(jsonPath("$.data").value(NAME_VALUE.trim()));
+    }
+
+    @Test
+    void testRequestParamCollectionStringTrimByRequestParamAnnotation() throws Exception {
+        // 测试接口有'RequestParam'注解的'MultiValueMap<String,String>'参数去空格是否有效
+        String url = Constant.PREFIX_SERVLET_PATH + "/requestParamCollectionStringTrimByRequestParamAnnotation";
+        final String value2 = " gengshao  ";
+        mockMvc.perform(get(url).param(COLL_NAME, NAME_VALUE, value2))
+                .andExpect(jsonPath("$.data[0]").value(NAME_VALUE.trim()))
+                .andExpect(jsonPath("$.data[1]").value(value2.trim()));
     }
 
     @RestController
@@ -80,12 +96,33 @@ class SecurityRequestParamMethodArgumentResolverTests {
             return Result.OK(name);
         }
 
-        private void printLog(String name) {
-            if (name == null) {
-                logger.info("入参：null");
+        private void printLog(Object obj) {
+            if (obj == null) {
+                logger.info("param: null");
+            } else if (obj instanceof String) {
+                logger.info("param: '{}'，String length after trim: {}", obj, obj.toString().length());
             } else {
-                logger.info("入参：'{}'，长度：{}", name, name.length());
+                logger.info("param: {}", obj);
             }
+        }
+
+        @RequestMapping(value = "/requestParamStringTrimByRequestParamAnnotationWithName", method = RequestMethod.GET)
+        public Result<?> requestParamStringTrimByRequestParamAnnotationWithName(@RequestParam(name = "name") String name,
+                                                                                Integer id) {
+            printLog(name);
+            printLog(id);
+            HashMap<String, Object> map = new HashMap<>(5);
+            map.put("id", id);
+            map.put("name", name);
+            return Result.OK(map);
+        }
+
+        @RequestMapping(value = "/requestParamCollectionStringTrimByRequestParamAnnotation", method = RequestMethod.GET)
+        public Result<?> requestParamCollectionStringTrimByRequestParamAnnotation(@RequestParam(name = COLL_NAME) Collection<Object> coll) {
+            for (Object value : coll) {
+                logger.info("Collection value: '{}', String length after trim: {}", value, value.toString().length());
+            }
+            return Result.OK(coll);
         }
     }
 }
