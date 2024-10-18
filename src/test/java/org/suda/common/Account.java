58e0069fcbd738610a1d8ad5f78a4d1aecc46e69
chengshaozhuang <chengsz7890@gmail.com>
2024-10-18 16:32:10 +0800

    添加用于测试的包装类

---
+++ src/test/java/org/suda/common/Account.java
@@ -0,0 +1,69 @@
+package org.suda.common;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import java.time.LocalDate;
+
+/**
+ * @author chengshaozhuang
+ * @dateTime 2024-10-18 15:52
+ */
+public class Account implements java.io.Serializable {
+    private Long sn;
+    private String id;
+    private String name;
+    private String email;
+    private LocalDate birthday;
+
+    public Long getSn() {
+        return sn;
+    }
+
+    public void setSn(Long sn) {
+        this.sn = sn;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @JsonSerialize(using = CustomLocalDateSerializer.class)
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "sn=" + sn +
+                ", id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", birthday='" + birthday.toString() +'\''+
+                '}';
+    }
+}
