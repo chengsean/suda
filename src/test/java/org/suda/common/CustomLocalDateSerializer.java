a04d32a4b7157ff913739566d66fffe88c5cd4da
chengshaozhuang <chengsz7890@gmail.com>
2024-10-18 16:37:50 +0800

    添加用于测试的工具类

---
+++ src/test/java/org/suda/common/CustomLocalDateSerializer.java
@@ -0,0 +1,27 @@
+package org.suda.common;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @author chengshaozhuang
+ * @dateTime 2024-10-18 16:18
+ */
+public class CustomLocalDateSerializer extends StdSerializer<LocalDate> {
+
+    protected CustomLocalDateSerializer() {
+        super(LocalDate.class);
+    }
+
+    @Override
+    public void serialize(LocalDate value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+        if (value != null) {
+            gen.writeString(value.format(DateTimeFormatter.ISO_LOCAL_DATE));
+        }
+    }
+}
