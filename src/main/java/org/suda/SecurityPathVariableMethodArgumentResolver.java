2a7bb356d090a62d60ccea9ab182a8796db34ff4
chengshaozhuang <chengsz7890@gmail.com>
2024-10-19 23:11:42 +0800

    改为重写‘resolveName’方法

--- src/main/java/org/suda/SecurityPathVariableMethodArgumentResolver.java
+++ src/main/java/org/suda/SecurityPathVariableMethodArgumentResolver.java
@@ -4,7 +4,6 @@ import org.suda.handler.MethodArgumentHandler;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.context.request.NativeWebRequest;
-import org.springframework.web.method.support.ModelAndViewContainer;
 import org.springframework.web.servlet.mvc.method.annotation.PathVariableMethodArgumentResolver;
 
 import javax.servlet.http.HttpServletRequest;
@@ -25,9 +24,9 @@ public class SecurityPathVariableMethodArgumentResolver extends PathVariableMeth
     }
 
     @Override
-    protected void handleResolvedValue(Object arg, String name, MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest request) {
+    protected Object resolveName(String name, MethodParameter parameter, NativeWebRequest request) throws Exception {
+        Object arg = super.resolveName(name, parameter, request);
         HttpServletRequest servletRequest = request.getNativeRequest(HttpServletRequest.class);
-        arg = stringMethodArgumentHandler.securityChecks(arg, servletRequest, parameter);
-        super.handleResolvedValue(arg, name, parameter, mavContainer, request);
+        return stringMethodArgumentHandler.securityChecks(arg, servletRequest, parameter);
     }
 }
