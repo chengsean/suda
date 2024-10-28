3a15a667f65f5312a69066cf31d20457204987ba
chengshaozhuang <chengsz7890@gmail.com>
2024-10-28 17:16:36 +0800

    suda-spring-boot-starter-sample添加公共类

---
+++ suda-spring-boot-starter-sample/src/main/java/org/suda/sample/common/Constant.java
@@ -0,0 +1,16 @@
+package org.suda.sample.common;
+
+/**
+ * 常量类
+ * @author chengshaozhuang
+ */
+public interface Constant {
+    /** {@code 200 OK} (HTTP/1.0 - RFC 1945) */
+    int SC_OK = 200;
+    /** 测试接口前缀 */
+    String PREFIX_SERVLET_PATH = "/test";
+
+    String NAME_KEY = "name";
+
+    String NAME_VALUE = " chengshaozhuang   ";
+}
