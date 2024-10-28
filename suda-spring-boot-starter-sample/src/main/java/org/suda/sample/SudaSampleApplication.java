1e550be8acd93460f60e8ea799816d5be3eba786
chengshaozhuang <chengsz7890@gmail.com>
2024-10-28 15:09:51 +0800

    初始化suda-spring-boot-starter-sample模块

---
+++ suda-spring-boot-starter-sample/src/main/java/org/suda/sample/SudaSampleApplication.java
@@ -0,0 +1,16 @@
+package org.suda.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chengshaozhuang
+ * @dateTime 2024-10-27 22:14
+ */
+@SpringBootApplication
+public class SudaSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SudaSampleApplication.class, args);
+    }
+}
